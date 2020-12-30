7ed15d56465883458a24fc10bceeafd5bd79323f
el10savio <el10savio96@gmail.com>
2020-12-30 12:50:04 +0530

    Refactored woot_errors.go

--- woot/woot_errors.go
+++ woot/woot_errors.go
@@ -3,7 +3,15 @@ package woot
 import "errors"
 
 var (
+	// ErrPositionOutOfBounds is the error message sent
+	// when the position out of bounds
 	ErrPositionOutOfBounds = errors.New("position out of bounds")
-	ErrEmptyWCharacter     = errors.New("empty wcharacter ID provided")
-	ErrBoundsNotPresent    = errors.New("subsequence bound(s) not present")
+
+	// ErrEmptyWCharacter is the error message sent
+	// when an empty wcharacter is ID provided
+	ErrEmptyWCharacter = errors.New("empty wcharacter ID provided")
+
+	// ErrBoundsNotPresent is the error message sent
+	// when subsequence bound(s) are not present
+	ErrBoundsNotPresent = errors.New("subsequence bound(s) not present")
 )
